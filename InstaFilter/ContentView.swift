5dfe6e162c0e8725ccb91119db5d19a75a8c9a86
Nick Rice <enprice383@gmail.com>
2021-08-05 17:42:40 +0100

    Initial Commit

---
+++ InstaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by Nick Rice on 05/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
